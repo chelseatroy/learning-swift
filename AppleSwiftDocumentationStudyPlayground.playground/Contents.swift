178afed47e4a423ecb7f661264af0fdef9ec671e
Chelsea Troy <chelsea.dommert@gmail.com>
2016-03-12 17:35:59 -0600

    Sets and Dictionaries

--- AppleSwiftDocumentationStudyPlayground.playground/Contents.swift
+++ AppleSwiftDocumentationStudyPlayground.playground/Contents.swift
@@ -75,4 +75,51 @@ for (index, value) in stringArray.enumerate() {
     print("Item \(index + 1): \(value)")
 }
 
+var myFavoriteMusic: Set = ["Rock", "Jazz", "Funk"]
+var yourFaveMusic: Set = ["Rock", "Rap", "Opera"]
+
+myFavoriteMusic.intersect(yourFaveMusic)
+myFavoriteMusic.exclusiveOr(yourFaveMusic)
+myFavoriteMusic.union(yourFaveMusic)
+myFavoriteMusic.subtract(yourFaveMusic)
+
+myFavoriteMusic.insert("Rap")
+myFavoriteMusic.insert("Opera")
+
+myFavoriteMusic.isSubsetOf(yourFaveMusic)
+myFavoriteMusic.isSupersetOf(yourFaveMusic)
+myFavoriteMusic.isStrictSubsetOf(yourFaveMusic)
+myFavoriteMusic.isStrictSupersetOf(yourFaveMusic)
+myFavoriteMusic.isDisjointWith(yourFaveMusic)
+
+var namesOfIntegers = [Int: String]()
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers = [:]
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+
+airports["APL"] = "Apple International"
+// "Apple International" is not the real airport for APL, so delete it
+airports["APL"] = nil
+// APL has now been removed from the dictionary
+
+if let removedValue = airports.removeValueForKey("DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain a value for DUB.")
+}
+
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+airports.keys
+airports.values
+let airportCodes = [String](airports.keys)
+
+
+
 
