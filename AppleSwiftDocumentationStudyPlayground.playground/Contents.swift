4288e5fc15ce50871963736b59d8382ea948b13a
Chelsea Troy <chelsea.dommert@gmail.com>
2016-04-18 16:16:03 -0500

    Nested Types

--- AppleSwiftDocumentationStudyPlayground.playground/Contents.swift
+++ AppleSwiftDocumentationStudyPlayground.playground/Contents.swift
@@ -633,7 +633,50 @@ for (_, snack) in favoriteSnacks {
 
 //Nested Types
 
+struct BlackjackCard {
+    
+    //example struct:
+    // nested Suit enumeration
+    enum Suit: Character {
+        case Spades = "♠", Hearts = "♡", Diamonds = "♢", Clubs = "♣"
+    }
+    
+    // nested Rank enumeration
+    enum Rank: Int {
+        case Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+        case Jack, Queen, King, Ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .Ace:
+                return Values(first: 1, second: 11)
+            case .Jack, .Queen, .King:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
+    // BlackjackCard properties and methods
+    let rank: Rank, suit: Suit
+    var description: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+    }
+}
+
+//to refer to a nested type outside of the context in which it is defined:
+let heartsSymbol = BlackjackCard.Suit.Hearts.rawValue
+// heartsSymbol is "♡"
 
 
+//Extensions
 
 
